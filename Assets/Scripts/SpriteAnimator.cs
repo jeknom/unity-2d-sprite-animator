b26ebbe1b59d4bb7369f9cd1143a0ffea3803457
Johannes Palvanen <johannes.palvanen@seriously.com>
2020-12-15 11:17:25 +0200

    Add sprite animator and example components

---
+++ Assets/Scripts/SpriteAnimator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteAnimator : MonoBehaviour
+{
+    [Serializable]
+    public class State
+    {
+        [SerializeField] bool isRepeated;
+        [SerializeField, Range(0f, 0.5f)] float animationFrameRateInSeconds = 0.1f;
+        [SerializeField] Sprite[] sprites;
+            
+        public bool IsRepeated => this.isRepeated;
+        public WaitForSeconds AnimationFrameRateInSeconds => new WaitForSeconds(this.animationFrameRateInSeconds);
+        public Sprite[] Sprites => this.sprites;
+    }
+
+    SpriteRenderer spriteRenderer;
+    State currentState;
+    
+    void Start()
+    {
+        this.spriteRenderer = this.GetComponent<SpriteRenderer>();
+    }
+    
+    public void PlayAnimation(State state)
+    {
+        if (state == this.currentState) return;
+            
+        this.StopAllCoroutines();
+        this.StartCoroutine(this.AnimationRoutine(state));
+    }
+
+    public void SetFlip(bool isFlipped)
+    {
+        if (this.spriteRenderer.flipX != isFlipped)
+        {
+            this.spriteRenderer.flipX = isFlipped;
+        }
+    }
+
+    IEnumerator AnimationRoutine(State animationState)
+    {
+        this.currentState = animationState;
+        var sprites = animationState.Sprites;
+        var waitForSeconds = animationState.AnimationFrameRateInSeconds;
+
+        do
+        {
+            for (var i = 0; i < sprites.Length; i++)
+            {
+                this.spriteRenderer.sprite = sprites[i];
+
+                yield return waitForSeconds;
+            }
+        } while (animationState.IsRepeated);
+    }
+}
---
+++ Assets/Sprite Animator Example/ExamplePlayerScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Sprite_Animator_Example
+{
+    [RequireComponent(typeof(SpriteAnimator))]
+    public class ExamplePlayerScript : MonoBehaviour
+    {
+        [SerializeField] SpriteAnimator.State idleAnimation;
+        [SerializeField] SpriteAnimator.State moveAnimation;
+
+        SpriteAnimator spriteAnimator;
+
+        void Start()
+        {
+            this.spriteAnimator = this.GetComponent<SpriteAnimator>();
+        }
+        
+        void Update()
+        {
+            var horizontalInput = Input.GetAxisRaw("Horizontal");
+
+            if (horizontalInput > 0f)
+            {
+                this.spriteAnimator.PlayAnimation(this.moveAnimation);
+                this.spriteAnimator.SetFlip(false);
+            }
+            else if (horizontalInput < 0f)
+            {
+                this.spriteAnimator.PlayAnimation(this.moveAnimation);
+                this.spriteAnimator.SetFlip(true);
+            }
+            else
+            {
+                this.spriteAnimator.PlayAnimation(this.idleAnimation);
+            }
+        }
+    }
+}
